12bc61758887044d2857e9360c2c4d580aca7a92
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-02-07 22:55:18 +0100

    design courses models

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+        
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=300)
+    description = models.TextField(max_length=5000)
+    image = models.ImageField(upload_to='courses/')
+    price = models.FloatField()
+    category = models.ForeignKey(Category, related_name='courses_category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
