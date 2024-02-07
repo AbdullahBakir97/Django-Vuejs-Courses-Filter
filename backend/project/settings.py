dd74b6195e975d2f2b72006321c238d4ca707610
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-02-07 22:58:38 +0100

    add  static & media settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
+    'rest_framework',
+    'django_filters',
+    'PIL',
+    
+    'courses',
+    
 ]
 
 MIDDLEWARE = [
@@ -116,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL =  '/media/'
+MEDIA_ROOT =  BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
