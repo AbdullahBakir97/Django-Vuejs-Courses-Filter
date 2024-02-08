ff377d29d2d74fd75c4370db69a6167a51a267c4
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-02-08 13:59:47 +0100

    add django rest swagger

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'django_filters',
+    'drf_yasg',
     'PIL',
     
     'courses',
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -21,8 +21,29 @@ from django.conf.urls.static import static
 
 from courses.views import CourseListAPI, CourseDetailAPI, CategoryListAPI
 
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Courses API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     
     path('api/courses/', CourseListAPI.as_view()),
     path('api/courses/<int:pk>/', CourseDetailAPI.as_view()),
